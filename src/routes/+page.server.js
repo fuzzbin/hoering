98490d7992c1413a1c910fdd3e14c68c3f8cf628
Tom Jarle Christiansen <fuzzbin@gmail.com>
2022-12-26 20:29:34 +0100

    Forbedret API

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -2,28 +2,33 @@ import { dbConnect, dbDisconnect } from '../utils/db';
 import { Sak } from '../models/saker';
 
 export const load = async () => {
-	console.log("Siden lastet på nytt!")
+	console.log('Siden lastet på nytt!');
 };
 
 export const actions = {
-  default: async (event) => {
-    // TODO log the user in
-    console.log("Her kommer dataene")
-	await dbConnect();
-	console.log('Hey hey!');
+	default: async ({ cookies, request }) => {
+		const data = await request.formData();
+		const t = data.get('tittel');
+		const b = data.get('beskrivelse');
+		console.log(t, b);
 
-	const nySak = new Sak({
-		sak_id: 'xxxx',
-		tittel: 'yyyy',
-		beskrivelse: 'zzz'
-	});
+		// Lagring til DB
+		await dbConnect();
+		console.log('Hey hey!');
 
-	nySak.save().then(() => console.log('meow!!!!!'));
+		const nySak = new Sak({
+			sak_id: 'xxxx',
+			tittel: t,
+			beskrivelse: b
+		});
 
-	await dbDisconnect();
-	console.log('Ho ho!');
-	return {
-		message: 'Her kommer høringene'
-	};
-  }
+		nySak.save().then(() => console.log('meow!!!!!'));
+
+		await dbDisconnect();
+		console.log('Ho ho!');
+		return {
+			message: 'Her kommer høringene',
+			success: true
+		};
+	}
 };
